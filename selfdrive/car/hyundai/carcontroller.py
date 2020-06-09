0dc732d5d0ef6c38e482c2fdc1901a5f32b8961e
AskAlice <alice@askalice.me>
2020-06-09 01:26:06 -0600

    ummmmmmm

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -129,7 +129,7 @@ class CarController():
       self.scc11_cnt += 1
       self.scc12_cnt += 1
       #cloudlog.info("create_scc12(self.packer, %d, %d, %d)" % (apply_accel, enabled, self.scc12_cnt))
-      #can_sends.append(create_scc12(self.packer,apply_accel, enabled, self.scc12_cnt))
+      can_sends.append(create_scc12(self.packer,apply_accel, enabled, self.scc12_cnt))
       can_sends.append(create_scc14(self.packer, enabled))
       #cloudlog.info("create_scc14(self.packer, %d)" % (enabled))
       
