fbcb2751f5a692c6b663a97aaf7b6887a4a1a05d
AskAlice <alice@askalice.me>
2020-06-16 17:47:43 -0600

    i don't wanna talk about it

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -74,12 +74,12 @@ class CarState(CarStateBase):
 
     #TODO: find pedal signal for EV/HYBRID Cars
     ret.gas = cp.vl["EMS12"]['PV_AV_CAN'] / 100
-    ret.gasPressed = bool(cp.vl["EMS16"]["CF_Ems_AclAct"])
-    if (cp.vl["TCS13"]["DriverOverride"] == 0 and cp.vl["TCS13"]['ACC_REQ'] == 1):
-      ret.pedal_gas = 0
-    else:
-      ret.pedal_gas = cp.vl["EMS12"]['TPS']
-    ret.car_gas = cp.vl["EMS12"]['TPS']
+     ret.gasPressed = bool(cp.vl["EMS16"]["CF_Ems_AclAct"])
+    # if (cp.vl["TCS13"]["DriverOverride"] == 0 and cp.vl["TCS13"]['ACC_REQ'] == 1):
+    #   ret.pedal_gas = 0
+    # else:
+    #   ret.pedal_gas = cp.vl["EMS12"]['TPS']
+    # ret.car_gas = cp.vl["EMS12"]['TPS']
     # TODO: refactor gear parsing in function
     # Gear Selection via Cluster - For those Kia/Hyundai which are not fully discovered, we can use the Cluster Indicator for Gear Selection,
     # as this seems to be standard over all cars, but is not the preferred method.
